7e29403f4f044bea592686a8525834c1f7665baf
yuliazhukova <yuliazhukova@hotmail.com>
2023-04-14 23:21:34 +0200

    including comments to test functions

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,21 +1,33 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 import pytest
+
 @pytest.fixture
 
 def item1():
     return Item("Смартфон", 10000, 20)
 
+
 def test_item_init(item1):
+
     """Функция, которая тестирует конструктор"""
+
     item1 = Item("Смартфон", 10000, 20)
     assert item1.name == 'Смартфон'
     assert item1.price == 10000
     assert item1.quantity == 20
 
+
 def test_item_calculate_total_price(item1):
+
+    """Функция, которая тестирует функцию calculate_total_price"""
+
     assert item1.calculate_total_price() == 200000
 
+
 def test_item_apply_discount(item1):
+
+    """Функция, которая тестирует функцию apply_discount"""
+
     item1.apply_discount()
     assert item1.price == 10000.0
